fff1a1e356e41d57c85e329a04b4af278ac8dc78
Adrian <adrian.mael.jung@gmail.com>
2023-12-25 20:20:01 +0100

    correct small mistake of parsing in case of default value

--- arachnida/spider.py
+++ arachnida/spider.py
@@ -1,16 +1,30 @@
 import argparse
-
-extensions = [".jpg", ".jpeg", ".png", ".gif", ".bmp"]
+import requests
+import sys
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("-r", action="store_true", help="recursively downloads the images from an URL")
-parser.add_argument("-l", type=int, dest="N", nargs="?", default=5, help="indicates the maximum depth level of the recursive download. If not indicated, it will be 5. -r flag is required")
-parser.add_argument("-p", type=str, dest="PATH",nargs="?", default="./data/",  help="indicates the path where the downloaded files will be saved. If not specified, ./data/ will be used.")
+parser.add_argument("-r", action="store_true",
+					help="recursively downloads the images from an URL")
+parser.add_argument("-l", type=int, dest="N", nargs="?", default=5,
+					help="""indicates the maximum depth level of the recursive download.
+					If not indicated, it will be 5. -r flag is required""")
+parser.add_argument("-p", type=str, dest="PATH",nargs="?", default="./data/",
+					help="""indicates the path where the downloaded files will be saved.
+					If not specified, ./data/ will be used.""")
 parser.add_argument("URL", help="indicates the URL from where to extract data")
+
 args = parser.parse_args()
 
-if (args.N and not args.r):
+if (sys.argv.__contains__("-l") and not sys.argv.__contains__("-r")):
 	parser.error("-r is required to use -l")
 
 print(args)
+
+#-------------------------------------------------------------------------------------------------#
+
+extensions = [".jpg", ".jpeg", ".png", ".gif", ".bmp"]
+
+# response = requests.get(args.URL)
+
+# open(args.PATH + "img1", "r").write(response.content)
