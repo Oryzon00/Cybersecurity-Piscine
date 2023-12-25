8e17eb4bb1a98dfd9117a23c04de5db1ca6ef0aa
Adrian <adrian.mael.jung@gmail.com>
2023-12-25 18:30:54 +0100

    finish parsing arg for spider.py

--- arachnida/spider.py
+++ arachnida/spider.py
@@ -1,48 +1,16 @@
-import sys
 import argparse
-
 
 extensions = [".jpg", ".jpeg", ".png", ".gif", ".bmp"]
 
-def program_info():
-	print("Here is how you can call this program: ./spider [-rlp] URL\n")
-	print("Option -r : recursively downloads the images in a URL")
-	print("Option -r -l [N] : indicates the maximum depth level of the recursive download. If not indicated, it will be 5.")
-	print("Option -p [PATH] : indicates the path where the downloaded files will be saved. If not specified, ./data/ will be used.")
-
-# print("script name:", sys.argv[0])
-
 parser = argparse.ArgumentParser()
-parser.add_argument("echo", help="echo your string", type="string")
+
+parser.add_argument("-r", action="store_true", help="recursively downloads the images from an URL")
+parser.add_argument("-l", type=int, dest="N", nargs="?", default=5, help="indicates the maximum depth level of the recursive download. If not indicated, it will be 5. -r flag is required")
+parser.add_argument("-p", type=str, dest="PATH",nargs="?", default="./data/",  help="indicates the path where the downloaded files will be saved. If not specified, ./data/ will be used.")
+parser.add_argument("URL", help="indicates the URL from where to extract data")
 args = parser.parse_args()
 
-print(args.echo)
+if (args.N and not args.r):
+	parser.error("-r is required to use -l")
 
-# print(args.filename, args.count, args.verbose)
-
-# len = len(sys.argv) - 1
-
-# if (len == 0):
-# 	print("wrong number of args")
-# 	program_info()
-# 	exit
-
-
-# for i in range(1, len(sys.argv)):
-# 	print(sys.argv[i])
-
-# program_info()
-
-
-
-# import argparse
-
-# parser = argparse.ArgumentParser(description="My Program")
-# parser.add_argument('-r', '--required', help='An optional argument')
-
-# args = parser.parse_args()
-
-# if args.required:
-#    print("Optional argument provided:", args.required)
-# else:
-#    print("No optional argument provided")
+print(args)
