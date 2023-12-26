08083512627bc5a624ba3f0507f70af8613923ed
Adrian <adrian.mael.jung@gmail.com>
2023-12-27 00:49:52 +0100

    recursing scrapping and download ding works

--- arachnida/spider.py
+++ arachnida/spider.py
@@ -3,12 +3,10 @@ import requests
 import sys
 import os
 from lxml import html
+
 from urllib.parse import urlsplit, urlparse
 
-
-
 parser = argparse.ArgumentParser()
-
 parser.add_argument("-r", action="store_true",
 					help="recursively downloads the images from an URL")
 parser.add_argument("-l", type=int, dest="N", nargs="?", default=5,
@@ -18,24 +16,12 @@ parser.add_argument("-p", type=str, dest="PATH",nargs="?", default="./data/",
 					help="""indicates the path where the downloaded files will be saved.
 					If not specified, ./data/ will be used.""")
 parser.add_argument("URL", help="indicates the URL from where to extract data")
-
 args = parser.parse_args()
-
-if (sys.argv.__contains__("-l") and not sys.argv.__contains__("-r")):
-	parser.error("-r is required to use -l")
-
-print("\n")
-print(args)
-print("\n")
-
-# print("\n----------------------------------------\n")
 
 #-------------------------------------------------------------------------------------------------#
 
-os.makedirs(name=args.PATH, exist_ok=True)
-
-def get_img_url(src: str) -> str:
-	parts_url = urlsplit(args.URL)
+def get_img_url(src: str, url: str) -> str:
+	parts_url = urlsplit(url)
 	#absolute url
 	if (urlparse(src).scheme):
 		return (src)
@@ -50,30 +36,67 @@ def download_img(url: str) -> None:
 	response_img = requests.get(url)
 	filename = os.path.basename(url)
 	open(args.PATH + filename, "wb").write(response_img.content)
-	print("Downloaded " + filename + "\nfrom " + url + "\n")
+	print("Downloaded " + filename + "\nFrom " + url + "\n")
 
-def download_all_imgs_from_url(url: str) -> None:
-	response_page = requests.get(args.URL)
-	tree = html.fromstring(response_page.content)
-	imgs = tree.xpath("//img")
+def download_all_imgs_from_url(url: str, tree: html.HtmlElement) -> None:
+	print("\n----- DOWNLOADING IMAGES FROM : " + url + " -----\n")
+	imgs : [html.HtmlElement] = tree.xpath("//img")
 	extensions = [".jpg", ".jpeg", ".png", ".gif", ".bmp"]
 
 	for img in imgs:
 		src = img.get("src")
+		if (src is None):
+			continue
 		for extension in extensions:
 			if (src.endswith(extension)):
-				img_url = get_img_url(src)
+				img_url = get_img_url(src, url)
 				download_img(img_url)
 
+def	get_valid_url_from_href(href: str, url: str, url_list: [str]):
+	parts_url = urlsplit(url)
+	#Relative url
+	if (href.startswith("/")):
+		href = parts_url.scheme + "://" + parts_url.netloc + href
+		print("Scrapped " + href + "\nFrom " + url + "\n")
+		url_list.append(href)
+	#Absolute url but same base url
+	elif(href.startswith(parts_url.scheme + "://" + parts_url.netloc)):
+		url_list.append(href)
+	#Invalid href for url
+	else:
+		return
+
+def	get_url_list_from_anchors(url: str, tree: html.HtmlElement) -> [str]:
+	print("\n----- SCRAPPING ALL <a> FROM : " + url + " -----\n")
+
+	url_list = []
+	anchors = tree.xpath("//a")
+	for anchor in anchors:
+		href = anchor.get("href")
+		get_valid_url_from_href(href, url, url_list)
+	return (url_list)
+
 def	recursive_download(url:str, n: int):
-	print("recursive level: " + str(n))
-	download_all_imgs_from_url(url)
+	response_page = requests.get(url)
+	tree : html.HtmlElement = html.fromstring(response_page.content)
+
+	download_all_imgs_from_url(url, tree)
 	if (n <= 0):
 		return
-	#get new url
-	recursive_download(url, n - 1)
+	url_list = get_url_list_from_anchors(url, tree)
+	for new_url in url_list:
+		recursive_download(new_url, n - 1)
+
+#-------------------------------------------------------------------------------------------------#
+
+
+if (sys.argv.__contains__("-l") and not sys.argv.__contains__("-r")):
+	parser.error("-r is required to use -l")
+os.makedirs(name=args.PATH, exist_ok=True)
 
 if (args.r):
 	recursive_download(args.URL, args.N)
 else:
 	recursive_download(args.URL, 0)
+
+print("\n----- DONE -----\n")
