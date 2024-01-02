3bfb7a5d4bb99f910fb31b77ffc2c1870d95bf2f
Adrian <adrian.mael.jung@gmail.com>
2024-01-02 10:18:24 +0100

    parsing done, exception handle for reading file

--- ft_otp/ft_otp.py
+++ ft_otp/ft_otp.py
@@ -1,11 +1,49 @@
 import argparse
+import sys
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-g", dest="hexa_key", help="""Generate an encrypted key in key.hex from a
+group = parser.add_mutually_exclusive_group(required=True)
+
+group.add_argument("-g", dest="hexa_key", help="""Generate an encrypted key in key.hex from a
 					hexadecimal of at least 64 characters""")
-parser.add_argument("-k", dest="key", help=""""Generate a TOTP using the encrypted key""")
+group.add_argument("-k", dest="key", help="""Generate a TOTP using the encrypted key""")
 args = parser.parse_args()
 
 #-------------------------------------------------------------------------------------------------#
 
+# Generate encrypted key | -g
+
+def	generate_encrypted_key(filename: str):
+	print("-g: ", filename)
+	try:
+		with open(filename, 'r') as f:
+			key = f.read()
+		print("content: ", key)
+	except Exception as e:
+		print("Error: ", e)
+		sys.exit(1)
 	
+#-------------------------------------------------------------------------------------------------#
+
+# Generate TOTP | -k
+
+def	generate_TOTP(filename: str):
+	print("-k: ", filename)
+	try:
+		with open(filename, 'r') as f:
+			key = f.read()
+		print("content: ", key)
+	except Exception as e:
+		print("Error: ", e)
+		sys.exit(1)
+
+#-------------------------------------------------------------------------------------------------#
+
+def	main():
+	if (args.hexa_key is not None):
+		generate_encrypted_key(args.hexa_key)
+	elif (args.key is not None):
+		generate_TOTP(args.key)
+
+if __name__ == "__main__":
+	main()
