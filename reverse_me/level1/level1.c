57b289e14b3d4b7b1d7f0b18ead0d11bd3e83f61
Adrian <adrian.mael.jung@gmail.com>
2024-01-16 21:50:08 +0100

    level 1 clear

---
+++ reverse_me/level1/level1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	char	password = "__stack_check";
+	char	*input;
+	strcpy(password, "__stack_check");
+	printf("Please enter key: ");
+	scanf("%s", input);
+	if ( !strcmp(input, password) )
+    	printf("Good job.\n");
+	else
+    	printf("Nope.\n");
+	return 0;
+}
