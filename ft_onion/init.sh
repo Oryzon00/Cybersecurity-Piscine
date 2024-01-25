d7fc483b987632fcaf30b63a880cfbb45a054b17
Adrian <adrian.mael.jung@gmail.com>
2024-01-25 13:14:21 +0100

    comments for clarity and cat tor hostname in docker logs

--- ft_onion/init.sh
+++ ft_onion/init.sh
@@ -1,5 +1,12 @@
+#Tor service
 /etc/init.d/tor restart
-# service tor restart
-/usr/sbin/sshd;
-nginx -g "daemon off;"
 
+#ssh service
+/usr/sbin/sshd;
+
+#Path to tor hostname
+echo "tor hostname:"
+cat /var/lib/tor/my_website/hostname
+
+#nginx service
+nginx -g "daemon off;"
